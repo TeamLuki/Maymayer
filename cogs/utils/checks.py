fd2c66098bd7e7ed24a51e0976634f43e4236ad2
TheLBall <outofmario1000@gmail.com>
2017-12-25 14:40:15 -0500

    Update checks.py

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -3,7 +3,7 @@ import discord.utils
 import sys
 
 def is_owner_check(message):
-    return message.author.id == 'Replace this with the bot owners id'
+    return message.author.id == 'Replace this with the bot owners id' #add or 'bot owner id' to this, replacing bot owner id with the id of another bot owner if you would like multiple bot owners.
 
 def is_owner():
     return commands.check(lambda ctx: is_owner_check(ctx.message))
