7ba96b96fe4db1b79fdf45430722584ce399a9b8
Kanielyochanan <32210799+DeveloperTJ@users.noreply.github.com>
2018-01-16 16:31:37 -0600

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -8,12 +8,12 @@ class General():
         self.bot = bot
 
     @commands.command()
-    async def ping(self):
+    async def ping(self, ctx):
         '''Says pong. Used for tests.'''
-        await self.bot.say("Pong! :ping_pong:")
+        await ctx.send("Pong! :ping_pong:")
 
     #credits to NanoBot for a little bit of this
-    @commands.command(pass_context=True)
+    @commands.command()
     async def info(self, ctx):
         '''Shows info about Maymayer.'''
         pyver = ""
@@ -22,36 +22,37 @@ class General():
                 pyver += str(x)
             else:
                 pyver += str(x) + "."
-        if ctx.message.server is not None:
-            color = ctx.message.server.me.color
+        if ctx.message.guild is not None:
+            color = ctx.message.guild.me.color
         users = sum(1 for _ in self.bot.get_all_members())
         embed = discord.Embed(color=color, title="Maymayer Info", description="Created by Luki. Maintained by TheLBall.")
         embed.set_footer(text="wanna join my free giftcard giveaway?")
         embed.set_thumbnail(url=self.bot.user.avatar_url)
-        embed.add_field(name="Stats: ", value="Servers: **{}** • Users: **{}**".format(len(self.bot.servers), users))
+        embed.add_field(name="Stats: ", value="Servers: **{}** • Users: **{}**".format(len(self.bot.guilds), users))
         embed.add_field(name="Version: ", value="Maymayer: **2.0.1** • discord.py: **{}** • Python: **{}**".format(discord.__version__, pyver))
         embed.add_field(name="Other: ", value = "Discord: https://discord.gg/GzNH2sB")
-        await self.bot.say(embed=embed)
+        await ctx.send(embed=embed)
 
     @commands.command()
-    async def server(self):
+    async def server(self, ctx):
         '''Gives an invite to the official Maymayer server.'''
-        await self.bot.say("https://discord.gg/GzNH2sB")
+        await ctx.message.author.send("https://discord.gg/GzNH2sB")
+        await ctx.send("**I sent an invite to my server in your DMs.**")
 
     @commands.command(name = "shutdown", hidden = True)
     @checks.is_owner()
-    async def _shutdown(self):
+    async def _shutdown(self, ctx):
         '''Shuts the bot down'''
-        await self.bot.say("RIP <@367072821160968194> 2017-2017")
+        await ctx.send("RIP <@367072821160968194> 2017-2017")
         try:
             await self.bot.logout()
         except:
             log.warn("Maymayer couldn't die.")
 
     @commands.command()
-    async def invite(self):
+    async def invite(self, ctx):
         '''Gives a bot invite.'''
-        await self.bot.say("https://discordapp.com/oauth2/authorize?client_id=367072821160968194&scope=bot&permissions=2146958591")
+        await ctx.message.author.send("https://discordapp.com/oauth2/authorize?client_id=367072821160968194&scope=bot&permissions=2146958591")
 
 def setup(bot):
     bot.add_cog(General(bot))
