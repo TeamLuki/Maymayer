5f28fbeab6936bda891090579772ef7d507a6bf7
Kanielyochanan <32210799+DeveloperTJ@users.noreply.github.com>
2018-01-16 20:36:57 -0600

    Update checks.py

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -2,9 +2,10 @@ from discord.ext import commands
 import discord.utils
 import sys
 
-def is_owner_check(message):
-    return message.author.id == 'Replace this with the bot owners id'
+owners = [replace this text with your ID]
+
+def is_owner_check(ctx):
+    return ctx.message.author.id in owners
 
 def is_owner():
-    return commands.check(lambda ctx: is_owner_check(ctx.message))
-
+    return commands.check(is_owner_check)
